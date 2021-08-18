d2b1a10059dbd8505f94de37885b82d319c3c26d
Josias <87546094+Josyas@users.noreply.github.com>
2021-08-17 22:46:11 -0300

    Add files via upload

---
+++ ModoTexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace poo_static {
+
+    class ModoTexto {
+
+        static public String Leia() {
+            return Console.ReadLine();
+        }
+
+        static public String Leia(String texto) {
+            Console.Write(texto);
+            return Console.ReadLine();
+        }
+
+        static public void Escrever(string texto) {
+            Console.WriteLine(texto);
+        }
+
+        static public void Pausar() {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace poo_static {
+    class Program {
+        static void Main(string[] args) {
+
+            String nome;
+            // ModoTexto.Escrever("Qual é o seu nome: ");
+            nome = ModoTexto.Leia("Qual é o seu nome: ");
+            ModoTexto.Escrever("Alo, " + nome);
+            ModoTexto.Pausar();
+        }
+
+        
+    }
+}
